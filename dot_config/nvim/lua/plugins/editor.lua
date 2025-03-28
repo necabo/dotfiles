b899bf3c1f341958b3eec1677b90384afa88bb83
necabo <necabo@mailbox.org>
2025-03-28 09:25:59 +0100

    Add asciidoc preview plugin and update plugins

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -1,3 +1,9 @@
 return {
   { "folke/flash.nvim", enabled = false },
+  {
+    'tigion/nvim-asciidoc-preview',
+    ft = { 'asciidoc' },
+    build = 'cd server && npm install',
+    opts = {},
+  }
 }
