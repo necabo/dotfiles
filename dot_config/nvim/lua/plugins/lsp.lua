519f3a35731e34345fa7419270fb22d9d740e555
necabo <necabo@mailbox.org>
2026-04-13 15:33:47 +0200

    Change deprecated lspconfig setup

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -10,16 +10,19 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-      keys[#keys + 1] = { "gK", false }
-      keys[#keys + 1] = { "<c-k>", false, mode = "i" }
-      keys[#keys + 1] = { "gH", vim.lsp.buf.signature_help, desc = "Signature Help", has = "signatureHelp" }
-      keys[#keys + 1] =
-        { "<c-h>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "signatureHelp" }
-      keys[#keys + 1] = { "K", false }
-      keys[#keys + 1] = { "gh", vim.lsp.buf.hover, desc = "Hover" }
-    end,
+    opts = {
+      servers = {
+        ['*'] = {
+          keys = {
+            { "gK", false },
+            { "<c-k>", false, mode = "i" },
+            { "gH", vim.lsp.buf.signature_help, desc = "Signature Help", has = "signatureHelp" },
+            { "<c-h>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "signatureHelp" },
+            { "K", false },
+            { "gh", vim.lsp.buf.hover, desc = "Hover" },
+          }
+        }
+      }
+    }
   },
 }
